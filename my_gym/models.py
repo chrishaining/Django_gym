1f0db0e565b603c448b049d5a5d6cbc1f58c11d0
Chris <chrishaining@yahoo.co.uk>
2020-01-25 17:39:21 +0000

    final commit before taking a break

--- my_gym/models.py
+++ my_gym/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from datetime import datetime as dt
+from django.utils import timezone
+
 
 # Create your models here.
 class Member(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class Session(models.Model):
+    title = models.CharField(max_length=255)
+    instructor = models.ForeignKey(Instructor, default="", on_delete=models.CASCADE)
+    # date = models.DateTimeField(default=timezone.now)
+
+
--- my_gym/tests.py
+++ my_gym/tests.py
@@ -7,5 +7,44 @@ class MemberTestCase(TestCase):
     
     def test_member_saved(self):
         self.assertGreater(self.member1.pk, 0)
-
     
+    def test_member_first_name(self):
+        self.assertEquals(self.member1.first_name, "Ron")
+
+    def test_member_last_name(self):
+        self.assertEquals(self.member1.last_name, "Jones")
+
+class SessionTestCase(TestCase):
+    def setUp(self):
+        self.instructor1 = Instructor.objects.create(first_name = "Mazie", last_name = "Kinsella")
+        self.session1 = Session.objects.create(title = "Pilates", instructor=self.instructor1)
+        # self.session1 = Session.objects.create(title = "Pilates", instructor=self.instructor1, date="Aug. 27, 2020")
+        # self.session2 = Session.objects.create(title="Yoga")
+    
+    def test_session_saved(self):
+        self.assertGreater(self.session1.pk, 0)
+    
+    def test_session_name(self):
+        self.assertEquals(self.session1.title, "Pilates")
+
+    def test_session_can_have_instructor(self):
+        self.assertEquals(self.session1.instructor.pk, self.instructor1.pk)
+
+    def test_session_instructor_first_name(self):
+        self.assertEquals(self.session1.instructor.first_name, "Mazie")
+
+    # def test_session_has_date(self):
+    #     self.assertEquals(self.session1.date, "Aug. 27, 2020")
+
+class InstructorTestCase(TestCase):
+    def setUp(self):
+        self.instructor1 = Instructor.objects.create(first_name = "Mazie", last_name = "Kinsella")
+    
+    def test_instructor_saved(self):
+        self.assertGreater(self.instructor1.pk, 0)
+
+    def test_instructor_first_name(self):
+        self.assertEquals(self.instructor1.first_name, "Mazie")
+
+    def test_instructor_last_name(self):
+        self.assertEquals(self.instructor1.last_name, "Kinsella")
